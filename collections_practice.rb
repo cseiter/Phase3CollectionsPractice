78c31bd6b03d3509ce5432cca91564b37bf2ca1c
chris <cseiter@gmail.com>
2021-07-05 20:46:05 -0400

    copy desc method and adjust to pass aort_array_char_count method

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,3 +7,7 @@ def sort_array_desc(array)
     array_copy = array.clone
     array_copy.sort {|x,y| -(x <=> y)}
 end
+
+def sort_array_char_count(array)
+    array.sort {|x,y| x.length <=> y.length}
+end
