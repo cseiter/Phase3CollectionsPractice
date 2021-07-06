c578cb3294241c51bddbf3e700f1013ce8f32906
chris <cseiter@gmail.com>
2021-07-05 20:49:10 -0400

    swap_elements passed using slicing

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,3 +11,8 @@ end
 def sort_array_char_count(array)
     array.sort {|x,y| x.length <=> y.length}
 end
+
+def swap_elements(array)
+    array[1],array[2] = array[2],array[1]
+    return array
+end
